c518ad461822c479efd7e719d81d6bf9c0902ed5
Lirovsk <araujojv04@yahoo.com>
2026-04-28 15:54:20 -0300

    Updating gitignore cache

--- src/main.py
+++ src/main.py
@@ -3,15 +3,37 @@
 # This is the main file for the project created using the file manager.
 
 
-from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
+from flask import Flask
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join('diobank.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+
+    import db
+    
+    db.init_app(app)
+
+
+    return app
