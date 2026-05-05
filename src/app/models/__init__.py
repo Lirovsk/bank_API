52d04df5ca99b3ed845b85d3a90396d2c2ff52e9
Lirovsk <araujojv04@yahoo.com>
2026-05-05 20:45:58 -0300

    Updating: fixing the models imports, fixing the definition of the anotted type, and fixin the other imports

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -1,10 +1,9 @@
-import uuid 
+import uuid
 
 from sqlalchemy.orm import mapped_column
-from sqlalchemy import UUID
+from typing_extensions import Annotated
 
-from  typing_extensions import Annotated
+id_uuid = Annotated[uuid.UUID, mapped_column(primary_key=True, default=uuid.uuid4)]
 
-
-id_uuid = Annotated[uuid.UUID,
-                    mapped_column(primary_key=True, default=uuid.uuid4)]
+from .user_account import User, Account
+from .transaction import Transaction
