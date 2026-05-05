5c733798ed68e69df8799b82ec38c4b3d45b80f5
Lirovsk <araujojv04@yahoo.com>
2026-05-05 20:49:29 -0300

    Fixing: fixing the wrong type passed to Mapped on line 16 and 17

--- src/app/models/transaction.py
+++ src/app/models/transaction.py
@@ -1,19 +1,21 @@
-from app import db
-from .account import Account
+from .. import db
 
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String, Integer, Numeric, DateTime, ForeignKey
 from typing import List, Optional
+from decimal import Decimal
 
 from datetime import datetime
 
+
 class Transaction(db.Model):
-    __tablename__ = 'transactions'
-    
+    __tablename__ = "transactions"
+
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     type: Mapped[str] = mapped_column(String(50), nullable=False)
-    amount: Mapped[Numeric] = mapped_column(nullable=False)
-    date: Mapped[DateTime] = mapped_column(nullable=False, default=datetime.now(tz=None))
+    amount: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
+    date: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, default=datetime.now
+    )
     account_maker_id: Mapped[str] = mapped_column(String(50), nullable=False)
     account_receiver_id: Mapped[str] = mapped_column(String(50), nullable=True)
-    
