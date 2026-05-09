0e396425bf6985057c7a09816c28ab3763457a21
Lirovsk <araujojv04@yahoo.com>
2026-05-08 21:01:53 -0300

    Updating: remodleing the user and account info and create another file to be the model for the account class

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -5,5 +5,7 @@ from typing_extensions import Annotated
 
 id_uuid = Annotated[uuid.UUID, mapped_column(primary_key=True, default=uuid.uuid4)]
 
-from .user_account import User, Account
+from .user_account import User
+from .user_account import Account as AccountInfo
 from .transaction import Transaction
+from .account import Account
--- src/app/models/user_account.py
+++ src/app/models/user_account.py
@@ -16,14 +16,14 @@ from datetime import date
 class User(db.Model):
     __tablename__ = "users"
 
-    id: Mapped[str] = mapped_column(
-        String(36), primary_key=True, default=lambda: str(uuid.uuid4())
-    )
-    name: Mapped[str] = mapped_column(String(50), nullable=False)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    accounts: Mapped[List["Account"]] = relationship(
-        "Account", back_populates="user", cascade="all, delete-orphan"
-    )
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(100), nullable=False)
+    age: Mapped[int] = mapped_column(Integer, nullable=False)
+    cpf: Mapped[str] = mapped_column(String(11), nullable=False, unique=True)
+    
+    accounts: Mapped[List["Account"]] = relationship("Account", back_populates="user")
 
     def __repr__(self):
         return f"<User: {self.name} ({self.email})>"
@@ -32,18 +32,12 @@ class User(db.Model):
 class Account(db.Model):
     __tablename__ = "accounts"
 
-    id: Mapped[id_uuid]
-    type: Mapped[str] = mapped_column(String(50), nullable=False)
-    balance: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
-    last_transaction_date: Mapped[date] = mapped_column(
-        Date, nullable=False, default=date.today
-    )
-    transaction_counter: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
-    user_id: Mapped[str] = mapped_column(
-        String(36), ForeignKey("users.id"), nullable=False
-    )
-
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    account_type: Mapped[str] = mapped_column(String(50), nullable=False)
+    account_number: Mapped[str] = mapped_column(String(20), nullable=False, unique=True)
+    
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=False)
     user: Mapped["User"] = relationship("User", back_populates="accounts")
-
+    
     def __repr__(self):
-        return f"<Account: {self.type} with balance {self.balance} for user {self.user.name}>"
+        return f"<Account: id: {self.id}, account_type: {self.account_type}, account_number: {self.account_number}"
