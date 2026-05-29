3828415441f0664fc6487bc22ecfaff1d6ef2322
João Vítor Lira <16088480602@estudante.ufjf.br>
2026-05-29 20:10:37 -0300

    feat: Addin the CRUD endpoints for the table game, all endpoints were simply tested

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -50,5 +50,7 @@ def create_app(test_config=None):
     app.cli.add_command(init_db_command)
     db.init_app(app)
     
+    from ..controllers import game
+    app.register_blueprint(game.app)
     
     return app
